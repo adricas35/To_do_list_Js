97ef92aa122192072de5cbead9e7cfde03574fa8
Adriana Castillo <adricasra1993@gmail.com>
2025-09-17 17:18:10 -0500

    Termine la fuction CreateToDoItem que genera el nodo padre de la tarea con sus hijos usando javaScript

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
 // Creamos las constantes globales para los elementos principales son globales porque no estan dentro de una fuction
 
-constInput= document.getElementById("to-do-input");
-constAddBtn= document.getElementById("add-btn");
-constToDoList= document.getElementById("cont-to-do-list");
-constCopletedList= document.getElementById("cont-copleted-list");
+const input= document.getElementById("to-do-input");
+const addBtn= document.getElementById("add-btn");
+const toDoList= document.getElementById("cont-to-do-list");
+const copletedList= document.getElementById("cont-copleted-list");
 
 
 // Cramos la funcion que nos permite crear una nueva tarea apartir del formulario 
 // Toda etiqueta que vamos a crear es apartir de la maqueta html pre-existente
-function CreateToDoItem(){
+function CreateToDoItem(textoItem){
 
-    // Creamos el nodo o elemento padre
+    // Creamos el nodo o elemento padre o contenedor
     const item= document.createElement('div');
     item.classList.add("item-to-do");
-// Creamos el nodo hijo del input y le agragamos el type checkbox
+
+    // Creamos el nodo hijo del input y le agragamos el type checkbox
     const checkbox= document.createElement('input');
     checkbox.type= "checkbox";
 
+    // Creamos el siguiente nodo hijo parrafo
+    const p= document.createElement("P");
+    // A este parrafo le asigno el valor del argumento de la  fuction, es decir lo que escribe el usuario en el campo
+    p.textContent= textoItem;
 
+    // Creamos el último nodo hijo, del boton de eliminar
+    const deleteBtn= document.createElement("button");
+    deleteBtn.textContent="x";
+
+    // Ensamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la tarea
+    item.appendChild(checkbox);
+    item.appendChild(p);
+    item.appendChild(deleteBtn);
+
+    // Utilizamos el return para retornar o dar respuesta del elemento creado ya que lo usaremos en otra funcion más adelante
+    return item;
 }
+
+
 
 
 
