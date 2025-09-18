e2a846668a35f1252c6b83c88c55ea7030ad4ba3
Adriana Castillo <adricasra1993@gmail.com>
2025-09-18 14:45:16 -0500

    Agregue la funcionalidad de tareas agregadas y por hacer ademas del eliminar tarea

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const addBtn= document.getElementById("add-btn");
 const toDoList= document.getElementById("cont-to-do-list");
 const copletedList= document.getElementById("cont-copleted-list");
 
-
+// Esta function solo crea la estructura html y la deja en limbo o memoria temporal para ser agregada luego mediante un appenChild
 // Cramos la funcion que nos permite crear una nueva tarea apartir del formulario 
 // Toda etiqueta que vamos a crear es apartir de la maqueta html pre-existente
 function CreateToDoItem(textoItem){
@@ -37,6 +37,8 @@ function CreateToDoItem(textoItem){
 }
 
 
+
+
 // Detectamos el click o el evento click sobre el boton con un evento de escucha sobre el boton agregar (+) 
 // Para que apartir de este evento se agregue la tarea dentro del contenedor
 
@@ -44,12 +46,42 @@ addBtn.addEventListener('click', ()=>{
     const textoItem= input.value.trim();
     if (textoItem=="") {
         alert= "No se puede crear una tarea vacia"; 
-    }else {
+    }
+    else {
         const newItem= CreateToDoItem(textoItem);
         toDoList.appendChild(newItem);
+        eventsToItem(newItem);
         input.value="";
     }
 });
+
+
+
+// La siguiente function nos permitira agregar el funcionamiento principal sobre las tareas es decir marcar la tarea como completada o en dado caso eliminarla
+function eventsToItem(item){
+    // utilizamos querySelector para capturar el input y button que estan del item
+    const checkbox = item.querySelector("input");
+    const deleteBtn= item.querySelector("button");
+    // completar la tarea
+    checkbox.addEventListener('change',()=>{
+        if (checkbox.checked) {
+            copletedList.appendChild(item);
+        }
+        else{
+            toDoList.appendChild(item);
+        }
+    });
+
+
+    // 
+    deleteBtn.addEventListener('click', ()=>{
+        item.remove();  
+    })
+}
+
+
+
+
 
 
 
